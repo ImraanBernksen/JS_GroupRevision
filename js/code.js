48fbc0cb2f4c8dfd14d79435e9e9630c51ad02c0
ImraanBernksen <imraanbernksen@gmail.com>
2022-12-02 09:14:44 +0200

    www

--- js/code.js
+++ js/code.js
@@ -218,16 +218,16 @@
 //     let numb2 = document.querySelector("#numb2").value;
 //     let output = document.querySelector('#output').innerText = numb1 + numb2;
 // }
-function addition(e) {
-    e.preventDefault();
-    let numb1 = document.querySelector("#numb1").value;
-    let numb2 = document.querySelector("#numb2").value;
-    document.querySelector('#output').innerText = eval(`${numb1} + ${numb2}`);
-}
+// function addition(e) {
+//     e.preventDefault();
+//     let numb1 = document.querySelector("#numb1").value;
+//     let numb2 = document.querySelector("#numb2").value;
+//     document.querySelector('#output').innerText = eval(`${numb1} + ${numb2}`);
+// }
 
-let btnAddition = document.querySelector('button');
-btnAddition.addEventListener('click', ()=>{
-    let numb1 = document.querySelector("numb1").value;
-    let numb2 = document.querySelector("numb1").value;
-    let output = document.querySelector("#output").innerText = parseInt(numb1) + parseInt(numb2);
-})
+// let btnAddition = document.querySelector('button');
+// btnAddition.addEventListener('click', ()=>{
+//     let numb1 = document.querySelector("numb1").value;
+//     let numb2 = document.querySelector("numb1").value;
+//     let output = document.querySelector("#output").innerText = parseInt(numb1) + parseInt(numb2);
+// })
